b12e4bad9bc18200de698ff22d014d597115a5e6
saharfk78 <sahar.kashany@yahoo.com>
2021-03-08 18:03:47 +0330

    adding GUI

---
+++ fileBrowser.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
+    QPushButton, QVBoxLayout, QFileDialog
+
+
+# you can copy and run this code
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        # Just initialization
+        super(MainWindow, self).__init__(parent)
+        self.setWindowTitle("open file dialog")
+
+        btn = QPushButton("Open File")
+        layout = QVBoxLayout()
+        layout.addWidget(btn)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        btn.clicked.connect(self.open)  # connect clicked to self.open()
+        self.show()
+
+    def open(self):
+        path = QFileDialog.getOpenFileName(self, 'Open a file', '',
+                                           'All Files (*.*)')
+        if path != ('', ''):
+            print("File path : " + path[0])
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
